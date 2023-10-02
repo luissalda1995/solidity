36477a855885ad37ec4c7a54ad8cd6e898e40ef8
luissalda1995 <luis.salda.1995@gmail.com>
2023-10-02 12:50:24 -0500

    Fallback

---
+++ contracts/Fallback.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Fallback {
+
+    mapping(address => uint) public usersBalance;
+
+    function deposit() public payable {
+        usersBalance[msg.sender] += msg.value;
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return usersBalance[msg.sender];
+    }
+
+    receive() external payable { }
+    
+    fallback() external payable {}
+}
